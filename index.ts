728ae85572ef7714b7f3531a10e1bd4d17f31281
Terrence James Diaz <terrence.diaz@laserfiche.com>
2022-05-18 23:50:47 -0700

    Make changes to exports

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { GetAccessTokenResponse } from "./lib/GetAccessTokenResponse.js";
 import { AccessKey } from "./lib/AccessKey.js";
 import { OAuthClientCredentialsHandler } from "./lib/OAuthClientCredentialsHandler.js";
+import { HttpRequestHandler } from "./lib/HttpRequestHandler.js";
 
-export { GetAccessTokenResponse, AccessKey, OAuthClientCredentialsHandler as ClientCredentialsGrantHandler};
+export { GetAccessTokenResponse, AccessKey, OAuthClientCredentialsHandler, HttpRequestHandler};
 export * as JwtUtils from './lib/util/JwtUtil.js';
