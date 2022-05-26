9c3b9b83bea674078826a58f66f7b5f85595eb2f
Steven Xu <steven.xu@laserfiche.com>
2022-05-26 16:13:17 -0400

    Revert "modify testHelper file"
    This reverts commit 58be1fad6732b8e695c6adcefec19e172cb1e46f.

--- lib/OAuth/TokenApiClient.test.ts
+++ lib/OAuth/TokenApiClient.test.ts
@@ -5,45 +5,40 @@ import { TokenApiClient } from './TokenApiClient.js';
 
 describe('getAccessToken', () => {
   let inst: TokenApiClient;
-  let accessKey:any = JSON.parse(testKey);
+
   test('Wrong domain returns null', async () => {
     let domain = 'fake.laserfiche.com';
     inst = new TokenApiClient(domain);
 
-    expect(async () => await inst.getAccessToken(testServicePrincipalKey, accessKey)).rejects.toThrow();
-  });
+    expect(async () => await inst.getAccessToken(testServicePrincipalKey, testKey)).rejects.toThrow();
+  })
 
   test('Malformed domain returns null', async () => {
     let domain = 'blah';
     inst = new TokenApiClient(domain);
 
-    expect(async () => await inst.getAccessToken(testServicePrincipalKey, accessKey)).rejects.toThrow();
-  });
+    expect(async () => await inst.getAccessToken(testServicePrincipalKey, testKey)).rejects.toThrow();
+  })
 
   test('Correct config returns access token', async () => {
-    let domain = accessKey.domain;
-    console.log(process.env);
-    let accessParse = `${accessKey.replace(/\\\\/g, '\\')}`;
-    console.log(accessParse);
-    let accessJson:AccessKey = JSON.parse(accessParse);
-    let domain2 = accessJson.domain;
-    console.log(accessJson.jwk);
-    inst = new TokenApiClient(domain2);
-    let result: GetAccessTokenResponse = await inst.getAccessToken(testServicePrincipalKey, accessJson);
-    console.log(result);
-    expect(result?.access_token).toBeTruthy();
-  });
-
-  test('Correct domain is case insensitive', async () => {
-    let domain = accessKey.domain.toUpperCase();
+    let domain = testKey.domain;
     inst = new TokenApiClient(domain);
 
-    let result: GetAccessTokenResponse = await inst.getAccessToken(testServicePrincipalKey, accessKey);
+    let result: GetAccessTokenResponse = await inst.getAccessToken(testServicePrincipalKey, testKey);
     expect(result?.access_token).toBeTruthy();
-  });
+  })
+
+
+  test('Correct domain is case insensitive', async () => {
+    let domain = testKey.domain.toUpperCase();
+    inst = new TokenApiClient(domain);
+
+    let result: GetAccessTokenResponse = await inst.getAccessToken(testServicePrincipalKey, testKey);
+    expect(result?.access_token).toBeTruthy();
+  })
 
   test('Empty domain throws exception', async () => {
     let domain = '';
     expect(() => new TokenApiClient(domain)).toThrow();
-  });
-});
+  })
+})
--- testHelper.ts
+++ testHelper.ts
@@ -5,6 +5,6 @@ export const testServicePrincipalKey: string =
 if (!testServicePrincipalKey) {
   throw new Error(`Unable to load DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY from .env`);
 }
-//let accessKey = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY;
-//accessKey = accessKey?.replace(/\\/g,'');
-export const testKey: string = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY ?? '';
+let accessKey = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY;
+accessKey = accessKey?.replace(/\\\"/g,'"');
+export const testKey: AccessKey = JSON.parse(accessKey ?? '');
