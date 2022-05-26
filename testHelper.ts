e887400720f54f77cba773b574fbf70911476451
Steven Xu <steven.xu@laserfiche.com>
2022-05-26 16:32:12 -0400

    updated typescript version

--- testHelper.ts
+++ testHelper.ts
@@ -6,5 +6,8 @@ if (!testServicePrincipalKey) {
   throw new Error(`Unable to load DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY from .env`);
 }
 let accessKey = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY;
-accessKey = accessKey?.replace(/\\\"/g,'"');
+console.log(process.env);
+const reg = /\\\"/g;
+accessKey = accessKey?.replace(reg,'"');
+console.log(accessKey);
 export const testKey: AccessKey = JSON.parse(accessKey ?? '');
