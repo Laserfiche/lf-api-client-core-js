ad687564a3ea9cb751855cbd765e2646b3ed8629
Steven Xu <steven.xu@laserfiche.com>
2022-05-26 18:34:41 -0400

    removed logs, comments, and reformatted

--- lib/OAuth/TokenApiClient.test.ts
+++ lib/OAuth/TokenApiClient.test.ts
@@ -1,25 +1,23 @@
-import {testKey,testServicePrincipalKey} from '../../testHelper';
+import { testKey, testServicePrincipalKey } from '../../testHelper';
 import { AccessKey } from './AccessKey.js';
 import { GetAccessTokenResponse } from './GetAccessTokenResponse.js';
 import { TokenApiClient } from './TokenApiClient.js';
 
 describe('getAccessToken', () => {
   let inst: TokenApiClient;
-  console.log(process.env.DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY);
-  console.log(process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY);
   test('Wrong domain returns null', async () => {
     let domain = 'fake.laserfiche.com';
     inst = new TokenApiClient(domain);
 
     expect(async () => await inst.getAccessToken(testServicePrincipalKey, testKey)).rejects.toThrow();
-  })
+  });
 
   test('Malformed domain returns null', async () => {
     let domain = 'blah';
     inst = new TokenApiClient(domain);
 
     expect(async () => await inst.getAccessToken(testServicePrincipalKey, testKey)).rejects.toThrow();
-  })
+  });
 
   test('Correct config returns access token', async () => {
     let domain = testKey.domain;
@@ -27,8 +25,7 @@ describe('getAccessToken', () => {
 
     let result: GetAccessTokenResponse = await inst.getAccessToken(testServicePrincipalKey, testKey);
     expect(result?.access_token).toBeTruthy();
-  })
-
+  });
 
   test('Correct domain is case insensitive', async () => {
     let domain = testKey.domain.toUpperCase();
@@ -36,10 +33,10 @@ describe('getAccessToken', () => {
 
     let result: GetAccessTokenResponse = await inst.getAccessToken(testServicePrincipalKey, testKey);
     expect(result?.access_token).toBeTruthy();
-  })
+  });
 
   test('Empty domain throws exception', async () => {
     let domain = '';
     expect(() => new TokenApiClient(domain)).toThrow();
-  })
-})
+  });
+});
--- testHelper.ts
+++ testHelper.ts
@@ -2,17 +2,12 @@ import 'dotenv/config';
 import { AccessKey } from './lib/OAuth/AccessKey.js';
 export const testServicePrincipalKey: string =
   process.env.DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY ?? '';
-//export const testServicePrincipalKey: string ="mbF_XhO9j9AmLIkbSawn";
 if (!testServicePrincipalKey) {
   throw new Error(`Unable to load DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY from .env`);
 }
-let accessKey:any = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY;
-console.log(process.env.DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY);
-console.log(process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY);
-//let accessKey:any = '{"customerId":"1123456789","domain":"a.clouddev.laserfiche.ca","clientId":"hSnhXJxuzNdWeYtQoE6wCici","jwk":{"kty":"EC","crv":"P-256","use":"sig","kid":"VT5Wz_yjvny0O66hqYFWv2mUAUjoNjLITJYzWGnkIn4","x":"6oZILnV7ytZPB1uz2P47_a_Ymko7SmTNuGpnzl20iCs","y":"ZQorDAQqhY6ojHSV_dpzXxbKI0eKljZbeGQKYDfPHsE","d":"a5Dv3pBpSk9myjg4CSEjNwV4hYx0IhFgqYwMl_TPB1E"}}';
-//console.log(accessKey);
+let accessKey: any = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY;
 const reg = /\\\"/g;
-accessKey = accessKey?.replace(reg,'"');
-accessKey = accessKey?.replace('"{','{');
-accessKey = accessKey?.replace('}"','}');
+accessKey = accessKey?.replace(reg, '"');
+accessKey = accessKey?.replace('"{', '{');
+accessKey = accessKey?.replace('}"', '}');
 export const testKey: AccessKey = JSON.parse(accessKey ?? '');
