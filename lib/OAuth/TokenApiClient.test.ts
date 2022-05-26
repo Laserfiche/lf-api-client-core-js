9bdc0239c94eb02b1fdd1fd14ab85ec07a4d189d
Steven Xu <steven.xu@laserfiche.com>
2022-05-26 16:42:33 -0400

    console log in test.ts file

--- lib/OAuth/TokenApiClient.test.ts
+++ lib/OAuth/TokenApiClient.test.ts
@@ -1,11 +1,13 @@
-import {testKey,testServicePrincipalKey} from '../../testHelper';
+import { access } from 'fs';
+import {accessKey,testServicePrincipalKey} from '../../testHelper';
 import { AccessKey } from './AccessKey.js';
 import { GetAccessTokenResponse } from './GetAccessTokenResponse.js';
 import { TokenApiClient } from './TokenApiClient.js';
 
 describe('getAccessToken', () => {
   let inst: TokenApiClient;
-
+  console.log(accessKey);
+  const testKey: AccessKey = JSON.parse(accessKey ?? '');
   test('Wrong domain returns null', async () => {
     let domain = 'fake.laserfiche.com';
     inst = new TokenApiClient(domain);
--- testHelper.ts
+++ testHelper.ts
@@ -5,9 +5,9 @@ export const testServicePrincipalKey: string =
 if (!testServicePrincipalKey) {
   throw new Error(`Unable to load DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY from .env`);
 }
-let accessKey = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY;
-console.log(process.env);
-const reg = /\\\"/g;
-accessKey = accessKey?.replace(reg,'"');
-console.log(accessKey);
-export const testKey: AccessKey = JSON.parse(accessKey ?? '');
+export const accessKey:any = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY;
+//console.log(accessKey);
+//const reg = /\\\"/g;
+//accessKey = accessKey?.replace(reg,'"');
+//console.log(accessKey);
+//export const testKey: AccessKey = JSON.parse(accessKey ?? '');
