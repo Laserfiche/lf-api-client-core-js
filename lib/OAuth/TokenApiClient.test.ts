eccb68f642b487389068a8dc5dc04354290d3b26
Steven Xu <steven.xu@laserfiche.com>
2022-05-26 11:17:22 -0400

    console log type

--- lib/OAuth/TokenApiClient.test.ts
+++ lib/OAuth/TokenApiClient.test.ts
@@ -24,6 +24,7 @@ describe('getAccessToken', () => {
 
   test('Correct config returns access token', async () => {
     let domain = accessKey.domain;
+    console.log(typeof(accessKey));
     console.log(Object.keys(accessKey));
     console.log(Object.values(accessKey));
     inst = new TokenApiClient(domain);
