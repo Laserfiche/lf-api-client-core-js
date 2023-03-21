28a61dfda629a91b82428cd488dec5af0666aa61
Matt Yin <matt.yin@laserfiche.com>
2023-03-21 13:15:30 -0400

    Trim the trailing slash for base url

--- lib/APIServer/TokenClient.test.ts
+++ lib/APIServer/TokenClient.test.ts
@@ -10,12 +10,6 @@ import { SessionKeyInfo } from './SessionKeyInfo.js';
  * @group IntegrationTests/SelfHosted
  */
 
-  /**
- * Debugging failing test
- *
- * @group IntegrationTests/Debug
- */
-
 describe('getAccessTokenFromAPIServer', () => {
   let inst: TokenClient;
   let body: CreateConnectionRequest = {
@@ -23,35 +17,36 @@ describe('getAccessTokenFromAPIServer', () => {
     username: username,
     password: password,
   };
-  // test('Wrong domain returns null', async () => {
-  //   let domain = 'fake.laserfiche.com';
-  //   inst = new TokenClient(domain);
-  //   expect(async () => await inst.createAccessToken(repositoryId, body)).rejects.toThrow();
-  // });
+  test('Wrong domain returns null', async () => {
+    let domain = 'fake.laserfiche.com';
+    inst = new TokenClient(domain);
+    expect(async () => await inst.createAccessToken(repositoryId, body)).rejects.toThrow();
+  });
 
-  // test('Malformed domain returns null', async () => {
-  //   let domain = 'blah';
-  //   inst = new TokenClient(domain);
-  //   expect(async () => await inst.createAccessToken(repositoryId, body)).rejects.toThrow();
-  // });
+  test('Malformed domain returns null', async () => {
+    let domain = 'blah';
+    inst = new TokenClient(domain);
+    expect(async () => await inst.createAccessToken(repositoryId, body)).rejects.toThrow();
+  });
   
   test('Correct config returns access token', async () => {
-    let domain = baseUrl;
+    let domain = baseUrl.endsWith('/') ? baseUrl.substring(0, baseUrl.length - 1) : baseUrl;
     inst = new TokenClient(domain);
 
     let result: SessionKeyInfo = await inst.createAccessToken(repositoryId, body);
     expect(result?.access_token).toBeTruthy();
   });
 
-  // test('Correct domain is case insensitive', async () => {
-  //   let domain = baseUrl.toUpperCase();
-  //   inst = new TokenClient(domain);
-  //   let result: SessionKeyInfo = await inst.createAccessToken(repositoryId, body);
-  //   expect(result?.access_token).toBeTruthy();
-  // });
+  test('Correct domain is case insensitive', async () => {
+    let domain = baseUrl.endsWith('/') ? baseUrl.substring(0, baseUrl.length - 1) : baseUrl;
+    let domainUpperCase = domain.toUpperCase();
+    inst = new TokenClient(domainUpperCase);
+    let result: SessionKeyInfo = await inst.createAccessToken(repositoryId, body);
+    expect(result?.access_token).toBeTruthy();
+  });
 
-  // test('Empty domain throws exception', async () => {
-  //   let domain = '';
-  //   expect(() => new TokenClient(domain)).toThrow();
-  // });
+  test('Empty domain throws exception', async () => {
+    let domain = '';
+    expect(() => new TokenClient(domain)).toThrow();
+  });
 });
