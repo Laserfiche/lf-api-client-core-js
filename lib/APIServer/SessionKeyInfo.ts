87434a466df22a336d9412873e2bdc7030234eee
Steven Xu <steven.xu@laserfiche.com>
2023-05-18 17:50:32 -0400

    remove source code and pipeline

--- lib/APIServer/SessionKeyInfo.ts
+++
@@ -1,5 +0,0 @@
-export interface SessionKeyInfo {
-  access_token: string | undefined;
-  expires_in: number | undefined;
-  token_type: string | undefined;
-}
--- lib/OAuth/GetAccessTokenResponse.ts
+++
@@ -1,7 +0,0 @@
-export interface GetAccessTokenResponse {
-  access_token: string | undefined;
-  expires_in: number | undefined;
-  token_type: string | undefined;
-  refresh_token: string | undefined;
-  scope: string | undefined;
-}
