7cba508e1f95d6eff07e0915f0330e4b963fdc40
Steven Xu <103658932+steven-xu-lf@users.noreply.github.com>
2023-05-24 10:26:26 -0400

    Update README.md (#62)
    * added 404.html file
    
    * remove source code and pipeline
    
    * updated readme file
    
    * removed additional extraneous files
    
    * remove .editorconfig file

--- lib/APIServer/CreateConnectionRequest.ts
+++
@@ -1,6 +0,0 @@
-export interface CreateConnectionRequest {
-    grant_type: string | undefined;
-    username: string | undefined;
-    password: string | undefined;
-  }
-  
--- lib/HttpHandlers/BeforeFetchResult.ts
+++
@@ -1,3 +0,0 @@
-export interface BeforeFetchResult {
-  regionalDomain: string;
-}
